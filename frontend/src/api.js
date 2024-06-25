f3519754756ef99f445a03a78df16690b6744e2b
Mohammed Rahil Hussain <mohammedrahilhussain@Mohammeds-MacBook-Pro.local>
2024-06-25 00:53:13 -0400

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,11 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
+const apiUrl = "/choreo-apis/stocksapp/backend/v1"
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+});
 
 api.interceptors.request.use(
     (config) => {
